e2dd7df9a3549837c9c3eb5819e172c0e6bf004d
Dominique Guillory <dguillory@smith.edu>
2024-09-14 21:31:42 -0400

    Fixed bugs, now program runs

--- computer.py
+++ computer.py
@@ -1,10 +1,41 @@
+# realization: I was getting multiple errors that didn't make sense to me (like indentation when I knew it was right), 
+# it turns out the problem was that I didn't declare the attributes or include pass in the constructor. Once I did that everything worked.
+
+
+
+
+
+
+
+
+
 class Computer:
 
-    # What attributes will it need?
+    # Attributes
+    description: str = ""
+    processor_type: str = ""
+    hard_drive_capacity: int = 0
+    memory: int = 0
+    operating_system: str = ""
+    year_made: int = 0
+    price: int = 0
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    # Constructor
+    def __init__(self, description: str, processor_type: str, hard_drive_capacity: int, memory: int, operating_system: str, year_made: int, price: int) -> None:
+        self.description = description
+        self.processor_type = processor_type
+        self.hard_drive_capacity = hard_drive_capacity
+        self.memory = memory
+        self.operating_system = operating_system
+        self.year_made = year_made
+        self.price = price
+        pass
 
-    # What methods will you need?
+
+def main():
+    newComputer: Computer = Computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, "macOS Big Sur", 2013, 1500)
+    print(newComputer.description)
+
+
+if __name__ == "__main__": 
+    main()
